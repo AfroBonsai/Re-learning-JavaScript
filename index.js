027cfe07f5865360ffeca1935d4dd96f2de7c9c8
AfroBonsai <geozam89@gmail.com>
2023-02-28 13:27:40 +0100

    VAT exercise

--- index.js
+++ index.js
@@ -33,38 +33,79 @@
 
 
 
-// ---------- -> Find out if a number is EVEN OR ODD with regular expression parse and integer check <- ---------- 
+// // ---------- -> Verify if a number is EVEN OR ODD with regular expression parse and integer check <- ----------
 
-let input = prompt("Enter a number");                   // Input window
 
-let pattern = /^\d*\.?\d*\S$/;                          // Regular expression to accept only numbers with decimal point and exclude spaces
+// let input = prompt("Enter a number")                    // Input window
 
-let inputCheck = pattern.test(input);                   // Variable used to check if the input is an accepted string
+// let pattern = /^\d*\.?\d*\S$/                           // Regular expression to accept only numbers with decimal point and exclude spaces
+
+// let inputCheck = pattern.test(input)                    // Variable used to check if the input is an accepted string
+
+// if (inputCheck === true) {
+
+//     let num = parseFloat(input)                         // Variable used to convert the string into number
+//     let integerCheck = Number.isInteger(num)            // Variable that checks if number is integer
+
+//     if (integerCheck === true) {
+
+//         let rest = (num % 2)                            // Variable to calculate the rest with % (can be 0 or 1 in this case)
+
+//         if (rest == 0) {                                // If there is NO rest the number is even.
+//             console.log("The number is EVEN")
+//         }
+//         else {
+//             console.log("the number is ODD")            // If there IS rest the number is odd.
+//         }
+
+//     }
+//     else {
+//         console.log("Incorrect input. Number needs to be Integer")       // Error in case the parsed number is not integer
+//     }
+// }
+
+// else {
+//     console.log("Incorrect input. Example: 3 or 2.6")   // Error in case the input does not match the regular expression
+// }
+
+
+// // ---------- -> END <- ----------
+
+
+
+
+// ---------- -> Ask for a price and calculate the VAT <- ----------
+
+
+let input = prompt("Introduce the price")
+
+let pattern = /^\d*\.?\d*\S$/
+
+let VAT = prompt("Introduce the VAT (%):")
+
+let patternVAT = /^(\d?[1-9]|[1-9]0)$/
+
+let inputCheck = pattern.test(input)                    // Variable used to check if the input is an accepted number
+
+let VATCheck = patternVAT.test(VAT)                     // Variable used to check if the input is an accepted number
 
 if (inputCheck === true) {
 
-    let num = parseFloat(input);                        // Variable used to convert the string into number
-    let integerCheck = Number.isInteger(num);           // Variable that checks if number is integer
+    if (VATCheck === true) {
+        let num = parseFloat(input)
+        let num2 = parseInt(VAT)
 
-    if (integerCheck === true) {
+        let result = num + (num * num2 / 100)
 
-        let rest = (num % 2);                           // Variable to calculate the rest with % (can be 0 or 1 in this case)
-
-        if (rest == 0) {                                // If there is NO rest the number is even.
-            console.log("The number is EVEN");
-        }
-        else {
-            console.log("the number is ODD");           // If there IS rest the number is odd.
-        }
-
+        console.log("Price introduced: " + input)
+        console.log("VAT percentage is: " + VAT)
+        console.log("Price with VAT is: " + result)
     }
-    else {
-        console.log("Incorrect input. Number needs to be Integer")       // Error in case the parsed number is not integer
-    }
-
+    else { console.log("Incorrect VAT. It has to be an integer number between 1-99.") }
 }
 
 else {
-    console.log("Incorrect input. Example: 3 or 2.6")   // // Error in case the input does not match the regular expression
-};
+    console.log("Introduce a correct price (use . as decimal point)")
+}
 
+// ---------- -> END <- ----------
